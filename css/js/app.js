844e5483821898dd56d7105890026342fa4b4928
playboiiro232-afk <playboiiro232@gmail.com>
2026-09-03 12:19:11 -0400

    Update app.js

--- css/js/app.js
+++ css/js/app.js
@@ -1,9 +1,27 @@
 function browseGames() {
-    document.querySelector(".games").scrollIntoView({
+    document.getElementById("games").scrollIntoView({
+        behavior: "smooth"
+    });
+}
+
+function browseProducts() {
+    document.getElementById("products").scrollIntoView({
         behavior: "smooth"
     });
 }
 
 function selectGame(game) {
     alert("You selected " + game + "!");
+}
+
+function addToCart(product, price) {
+    alert(product + " was added to your cart for $" + price.toFixed(2));
+}
+
+function openCart() {
+    alert("Your cart will be available soon!");
+}
+
+function contactSupport() {
+    alert("Bloxypay Support will be available soon!");
 }
